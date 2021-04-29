614508108e65d70e86a0faa809409e7fbec2e84b
Juli Arnalot <juli.arnalot@xing.com>
2021-04-29 10:38:58 +0200

    first commit

---
+++ pages/Speech1.js
@@ -0,0 +1,32 @@
+import useSpeechToText from "react-hook-speech-to-text";
+
+function Speech1() {
+  const {
+    error,
+    isRecording,
+    results,
+    startSpeechToText,
+    stopSpeechToText,
+  } = useSpeechToText({
+    continuous: true,
+    timeout: 10000,
+  });
+
+  if (error) return <p>Web Speech API is not available in this browser 🤷‍</p>;
+
+  return (
+    <div>
+      <h1>Recording: {isRecording.toString()}</h1>
+      <button onClick={isRecording ? stopSpeechToText : startSpeechToText}>
+        {isRecording ? "Stop Recording" : "Start Recording"}
+      </button>
+      <ul>
+        {results.map((result, index) => (
+          <li key={index}>{result}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Speech1;
